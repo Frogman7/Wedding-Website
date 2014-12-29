0df6e4fe8290e7ef8b636e0cf31d86868cecda51
Max Blumenthal <Thermonuclear@sbcglobal.net>
2014-12-28 18:59:08 -0800

    Added nodemailer as a means of email

--- config/emailer.js
+++ config/emailer.js
@@ -1,51 +1,51 @@
-var sendgrid = require('sendgrid')(
-	process.env.SENDGRID_USERNAME,
-	process.env.SENDGRID_PASSWORD
-);
-var Email = sendgrid.Email;
+var mailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
-exports.sendTestEmailTo = function(emailAddress, next) {
+var transporter = mailer.createTransport(smtpTransport({
+    transport: "SMTP",
+    host: "smtp.gmail.com",
+    secureConnection: true,
+    port: 587,
+    requiresAuth: true,
+    domains: ["gmail.com", "googlemail.com"],
+    auth: {
+        user: process.env.ACCOUNT,
+        pass: process.env.PASSWORD
+    }
+}));
 
-	sendgrid.send({
-		to: emailAddress,
-		from: process.env.EMAIL_FROM_ADDRESS,
-		subject: 'Hello world!',
-		text: 'Sending email with NodeJS through SendGrid.'
-	}, function(err, json) {
-	
-		if (err) {
-			console.error(err);
-			return next(err);
-		};
-		
-		next();
-	});
-};
+//var transporter = mailer.createTransport({
+//    service: 'Gmail',
+//    auth:
+//    {
+//        user: process.env.ACCOUNT,
+//        pass: process.env.PASSWORD
+//    }
+//});
 
 exports.sendRsvpLinkTo = function(emailAddress, rsvpId, next) {
 
-	var email = new Email({
-		to:       emailAddress,
-		from:     process.env.EMAIL_FROM_ADDRESS,
-		fromname: 'Charlene & Max',
-		subject:  "RSVP Link for Charlene & Max's Wedding"
-	});
-	
-	email.setHtml([
-		'<p>Hey there,</p>',
-		'',
-		'<p>',
-		'To get back into your RSVP, please use the following link:<br/>',
-		'<a href="http://www.sheaslounge.com/rsvp/detail/' + rsvpId + '">Update Your RSVP</a>',
-		'</p>',
-		'',
-		'<p>Please let Max know if you have any trouble.</p>',
-		'',
-		'<p>Thanks,<br/>',
-		'Max & Charlene</p>'
-	].join("\r\n"));
+    var email = {
+        from: 'Max Blumenthal <' + process.env.ACCOUNT + '>',
+        to: emailAddress, // list of receivers
+        subject: 'RSVP link for Max and Charlene\'s wedding', // Subject line
+        text: 'To get back into your RSVP, please use the following link: http://mcwedding.herokuapp.com/rsvp/detail/' + rsvpId, // plaintext body
+        html: [
+            '<p>Hey there,</p>',
+            '',
+            '<p>',
+            'To get back into your RSVP, please use the following link:<br/>',
+            '<a href="http://mcwedding.herokuapp.com/rsvp/detail/' + rsvpId + '">Update Your RSVP</a>',
+            '</p>',
+            '',
+            '<p>Please let Max know if you have any trouble.</p>',
+            '',
+            '<p>Thanks,<br/>',
+            'Max & Charlene</p>'
+        ].join("\r\n")
+    };
 
-	sendgrid.send(email, function(err, json) {
+    transporter.sendMail(email, function(err) {
 	
 		if (err) {
 			console.error(err);
@@ -58,37 +58,35 @@ exports.sendRsvpLinkTo = function(emailAddress, rsvpId, next) {
 
 exports.sendConfirmationFor = function(rsvp, next) {
 
-	var email = new Email({
-		to:       rsvp.emailAddress,
-		from:     process.env.EMAIL_FROM_ADDRESS,
-		fromname: 'Charlene & Max',
-		bcc:      process.env.EMAIL_FROM_ADDRESS,
-		subject:  "RSVP Confirmation for Charlene & Max's Wedding"
-	});
-	
-	email.setHtml([
-		'<p>Thanks for the RSVP!</p>',
-		'',
-		buildConfirmationContent(rsvp),
-		'',
-		'<p>',
-		'<em>Guests in Your Party</em>',
-		'<ul>',
-		'<li>' + rsvp.name + '</li>',
-		buildGuestList(rsvp),
-		'</ul>',
-		'</p>',
-		'',
-		'<p>',
-		'To get back into your RSVP, please use the following link:<br/>',
-		'<a href="http://www.sheaslounge.com/rsvp/detail/' + rsvp.rsvpId + '">Update Your RSVP</a>',
-		'</p>',
-		'',
-		'<p>Thanks,<br/>',
-		'Charlene & Max</p>'
-	].join("\r\n"));
+    var email = {
+        from: 'Max Blumenthal <' + process.env.ACCOUNT + '>',
+        to: emailAddress, // list of receivers
+        subject: 'RSVP confirmation for Max and Charlene\'s wedding', // Subject line
+        text: 'To get back into your RSVP, please use the following link: http://mcwedding.herokuapp.com/rsvp/detail/' + rsvpId, // plaintext body
+        html: [
+            '<p>Thanks for the RSVP!</p>',
+            '',
+            buildConfirmationContent(rsvp),
+            '',
+            '<p>',
+            '<em>Guests in Your Party</em>',
+            '<ul>',
+            '<li>' + rsvp.name + '</li>',
+            buildGuestList(rsvp),
+            '</ul>',
+            '</p>',
+            '',
+            '<p>',
+            'To get back into your RSVP, please use the following link:<br/>',
+            '<a href="http://mcwedding.herokuapp.com/rsvp/detail/' + rsvp.rsvpId + '">Update Your RSVP</a>',
+            '</p>',
+            '',
+            '<p>Thanks,<br/>',
+            'Charlene & Max</p>'
+        ].join("\r\n")
+    };
 
-	sendgrid.send(email, function(err, json) {
+	transporter.sendMail(email, function(err) {
 	
 		if (err) {
 			console.error(err);
@@ -104,7 +102,7 @@ function buildConfirmationContent(rsvp) {
 
 	if (rsvp.accept) {
 		return '<p>You said you were coming with a party of ' + rsvp.guestCount +
-		       '. See you on the beach!</p>';
+		       '. See you by the river!</p>';
 	} else if (rsvp.decline) {
 		return '<p>You said you couldn\'t make it. We\'ll miss you! Hopefully we will get a chance to catch up sometime.</p>';
 	} else {
