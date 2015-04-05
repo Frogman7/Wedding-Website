81d9cf5347adbd8adfc9a743da114ae536bad76a
ICodeFrog <Thermonuclear@sbcglobal.net>
2015-04-04 23:04:31 -0700

    Switched mailer back to sendgrid

--- config/emailer.js
+++ config/emailer.js
@@ -1,36 +1,32 @@
-var mailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+var sendgrid = require('sendgrid')(
+    process.env.SENDGRID_USERNAME,
+    process.env.SENDGRID_PASSWORD
+);
+var Email = sendgrid.Email;
 
-var transporter = mailer.createTransport(smtpTransport({
-    transport: "SMTP",
-    host: "smtp.gmail.com",
-    secureConnection: true,
-    port: 587,
-    requiresAuth: true,
-    domains: ["gmail.com", "googlemail.com"],
-    auth: {
-        user: process.env.ACCOUNT,
-        pass: process.env.PASSWORD
-    }
-}));
+exports.sendTestEmailTo = function (emailAddress, next) {
+    sendgrid.send({
+        to: emailAddress,
+        from: process.env.EMAIL,
+        subject: 'Hello world!',
+        text: 'Sending email with NodeJS through SendGrid.'
+    }, function (err, json) {
+        if (err) {
+            console.error(err);
+            return next(err);
+        };
+        next();
+    });
+};
 
-//var transporter = mailer.createTransport({
-//    service: 'Gmail',
-//    auth:
-//    {
-//        user: process.env.ACCOUNT,
-//        pass: process.env.PASSWORD
-//    }
-//});
-
-exports.sendRsvpLinkTo = function(emailAddress, rsvpId, next) {
-
-    var email = {
-        from: 'Max Blumenthal <' + process.env.ACCOUNT + '>',
-        to: emailAddress, // list of receivers
-        subject: 'RSVP link for Max and Charlene\'s wedding', // Subject line
-        text: 'To get back into your RSVP, please use the following link: http://mcwedding.herokuapp.com/rsvp/detail/' + rsvpId, // plaintext body
-        html: [
+exports.sendRsvpLinkTo = function (emailAddress, rsvpId, next) {
+    var email = new Email({
+        to: emailAddress,
+        from: process.env.EMAIL,
+        fromname: 'Charlene & Max',
+        subject: 'RSVP link for Max and Charlene\'s wedding'
+    });
+    email.setHtml([
             '<p>Hey there,</p>',
             '',
             '<p>',
@@ -42,28 +38,26 @@ exports.sendRsvpLinkTo = function(emailAddress, rsvpId, next) {
             '',
             '<p>Thanks,<br/>',
             'Max & Charlene</p>'
-        ].join("\r\n")
-    };
-
-    transporter.sendMail(email, function(err) {
-	
-		if (err) {
-			console.error(err);
-			return next(err);
-		};
-		
-		next();
-	});
+        ].join("\r\n"));
+    
+    sendgrid.send(email, function (err, json) {
+        if (err) {
+            console.error(err);
+            return next(err);
+        };
+        next();
+    });
 };
 
-exports.sendConfirmationFor = function(rsvp, next) {
-
-    var email = {
-        from: 'Max Blumenthal <' + process.env.ACCOUNT + '>',
-        to: rsvp.emailAddress, // list of receivers
-        subject: 'RSVP confirmation for Max and Charlene\'s wedding', // Subject line
-        text: 'To get back into your RSVP, please use the following link: http://mcwedding.herokuapp.com/rsvp/detail/' + rsvp.rsvpId, // plaintext body
-        html: [
+exports.sendConfirmationFor = function (rsvp, next) {
+    var email = new Email({
+        to: rsvp.emailAddress,
+        from: process.env.EMAIL,
+        fromname: 'Charlene & Max',
+        bcc: process.env.EMAIL,
+        subject: "RSVP Confirmation for Charlene & Max's Wedding"
+    });
+    email.setHtml([
             '<p>Thanks for the RSVP!</p>',
             '',
             buildConfirmationContent(rsvp),
@@ -83,39 +77,35 @@ exports.sendConfirmationFor = function(rsvp, next) {
             '',
             '<p>Thanks,<br/>',
             'Charlene & Max</p>'
-        ].join("\r\n")
-    };
-
-	transporter.sendMail(email, function(err) {
-	
-		if (err) {
-			console.error(err);
-			return next(err);
-		};
-		
-		next();
-	});
+        ].join("\r\n"));
+    
+    sendgrid.send(email, function (err, json) {
+        if (err) {
+            console.error(err);
+            return next(err);
+        };
+        next();
+    });
 };
-
 
 function buildConfirmationContent(rsvp) {
 
-	if (rsvp.accept) {
-		return '<p>You said you were coming with a party of ' + rsvp.guestCount +
-		       '. See you by the river!</p>';
-	} else if (rsvp.decline) {
-		return '<p>You said you couldn\'t make it. We\'ll miss you! Hopefully we will get a chance to catch up sometime.</p>';
-	} else {
-		return '<p>It looks like we may have had a problem saving whether you\'d be able to come or not. Please try again using the link below or contact Max.</p>';
-	}
+    if (rsvp.accept) {
+        return '<p>You said you were coming with a party of ' + rsvp.guestCount +
+            '. See you by the river!</p>';
+    } else if (rsvp.decline) {
+        return '<p>You said you couldn\'t make it. We\'ll miss you! Hopefully we will get a chance to catch up sometime.</p>';
+    } else {
+        return '<p>It looks like we may have had a problem saving whether you\'d be able to come or not. Please try again using the link below or contact Max.</p>';
+    }
 
-}
+};
 
 function buildGuestList(rsvp) {
 
-	if (rsvp.guests.length == 0) {
-		return '';
-	}
+    if (rsvp.guests.length == 0) {
+        return '';
+    }
 
-	return '<li>' + rsvp.guests.join("</li>\r\n<li>") + '</li>';
-}
+    return '<li>' + rsvp.guests.join("</li>\r\n<li>") + '</li>';
+};
